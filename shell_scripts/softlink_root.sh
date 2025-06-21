23a793d1dda8d573bb2bc526cebdfb135a489ea8
yunaimatsu <yunaimatsu@users.noreply.github.com>
2025-06-22 02:14:39 +0900

    WIP: Separate softlinking function from `setup.sh`

---
+++ shell_scripts/softlink_root.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+echo "Mount config files to home directory"
+  [KEYD_DEFAULT.conf]="/etc/keyd/default.conf"
+sudo chmod 777 "$HOME/dotfiles/KEYD_DEFAULT.conf"
--- shell_scripts/softlink_user.sh
+++ shell_scripts/softlink_user.sh
@@ -10,12 +10,10 @@ typeset -A files=(
   [TMUX_CONF]="$HOME/.tmux.conf"
   [NEOVIM_INIT.lua]="$HOME/.config/nvim/init.lua"
   [ESPANSO_MATCH.yml]="$HOME/.config/espanso/match/base.yml"
-  # [KEYD_DEFAULT.conf]="/etc/keyd/default.conf"
   [GIT_CONFIG]="$HOME/.gitconfig" 
   [HYPRLAND_CONFIG.conf]="$HOME/.config/hypr/hyprland.conf"
   [HYPRLAND_PAPER_CONFIG.conf]="$HOME/.config/hypr/hyprpaper.conf"
 )
-sudo chmod 777 "$HOME/dotfiles/KEYD_DEFAULT.conf"
 for src in "${(@k)files}"; do
   dest="${files[$src]}"
   echo "Linking $src -> $dest"
